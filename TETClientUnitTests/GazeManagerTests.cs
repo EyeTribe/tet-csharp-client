edda4065e74c2fb243772848b2ffe674e8b80da1
samanat <samanat@gmail.com>
2014-05-25 22:47:06 +0200

    samanat: adding basic structure for UnitTests

---
+++ TETClientUnitTests/GazeManagerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TETCSharpClient;
+
+namespace TETClientUnitTests
+{
+    [TestClass]
+    public class GazeManagerTests
+    {
+        [TestMethod]
+        public void HandleApiResponseTest()
+        {
+            GazeManager gz = GazeManager.Instance;
+            Assert.IsNotNull(gz);
+        }
+    }
+}
